263425cf0aa0cd3b5c1a3122db7c43fddc59bf4a
Violet <hanfovab@gmail.com>
2023-12-16 22:13:50 +0100

    backend removed, fetching data directly with react

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -7,40 +7,70 @@ export default function Posts({ sortOption, searchTerm }) {
     useEffect(() => {
         async function fetchPosts() {
             try {
-                const res = await fetch('http://localhost:8080/posts');
-                if (!res.ok) {
-                    throw new Error(`Error fetching posts: ${res.status}`)
+                const postsResponse = await fetch('https://jsonplaceholder.typicode.com/posts');
+                const usersResponse = await fetch('https://jsonplaceholder.typicode.com/users');
+                const commentsResponse = await fetch('https://jsonplaceholder.typicode.com/comments');
+
+                if (!postsResponse.ok || !usersResponse.ok || !commentsResponse.ok) {
+                    throw new Error('Error fetching data');
                 }
 
-                let posts = await res.json();
-                //sorting
+                const [posts, users, comments] = await Promise.all([
+                    postsResponse.json(),
+                    usersResponse.json(),
+                    commentsResponse.json(),
+                ]);
+
+                let postsData = posts.map((post) => {
+                    const user = users.find((user) => user.id === post.userId);
+
+                    const postComments = comments
+                        .filter((comment) => comment.postId === post.id)
+                        .map(({ id, name, email, body }) => ({ id, name, email, body }));
+
+                    return {
+                        id: post.id,
+                        title: post.title,
+                        body: post.body,
+                        user: user
+                            ? { id: user.id, name: user.name }
+                            : { id: null, name: 'Unknown User' },
+                        numComments: postComments.length,
+                        comments: postComments,
+                    };
+                });
+
+                // Sorting
                 if (sortOption === 'asc') {
-                    posts = posts.sort((a, b) => a.title.localeCompare(b.title));
+                    postsData.sort((a, b) => a.title.localeCompare(b.title));
                 } else if (sortOption === 'desc') {
-                    posts = posts.sort((a, b) => b.title.localeCompare(a.title));
+                    postsData.sort((a, b) => b.title.localeCompare(a.title));
                 }
-                //searching
-                if (searchTerm) {
-                    posts = posts.filter(
-                        (post) =>
-                            post.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
-                            post.body.toLowerCase().includes(searchTerm.toLowerCase())
-                    );
-                };
 
-                setLoadedPosts(posts);
+                // Searching
+                if (searchTerm) {
+                    const lowerCaseSearchTerm = searchTerm.toLowerCase();
+                    postsData = postsData.filter(
+                        (post) =>
+                            post.title.toLowerCase().includes(lowerCaseSearchTerm) ||
+                            post.body.toLowerCase().includes(lowerCaseSearchTerm)
+                    );
+                }
+
+                setLoadedPosts(postsData);
             } catch (err) {
-                console.error('Error fetching posts: ', err.message);
+                console.error('Error fetching data: ', err.message);
             }
         }
+
         fetchPosts();
     }, [sortOption, searchTerm]);
-    
+
     return (
         <ul className="list-unstyled">
             {loadedPosts.map((post) => (
                 <PostItem key={post.id} post={post} />
-            ))} 
+            ))}
         </ul>
     );
-};
+}
